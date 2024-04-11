f59302370de10d41efa30a17503e3065fa30b291
chrofire <451939778@qq.com>
2024-04-11 21:59:27 +0800

    docs: 更新文档样式

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,12 +5,13 @@ import { provideGlobalConfig } from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import Layout from './Layout.vue'
 
 /** @type {import('vitepress').Theme} */
 export default {
     extends: DefaultTheme,
     Layout () {
-        return h(DefaultTheme.Layout, null, {})
+        return h(Layout, null, {})
     },
     enhanceApp ({ app, router, siteData }) {
         provideGlobalConfig({
